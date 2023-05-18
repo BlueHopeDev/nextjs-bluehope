a872099f82b27cfd397a151ade962494eda5dda9
Akkharawit Somwong <akk.somwong@gmail.com>
2023-05-18 17:44:45 +0700

    fix: Loading component display

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -8,32 +8,35 @@ interface Props {
 }
 
 const Loading = ( {
-  duration = 2
+  duration = 2,
 }: Props ) => {
   
-  const [ timerCount, setTimerCount ] = useState<number>(duration);
-  const [ fadeDown, setFadeDown ] = useState<boolean>(false);
+  const [ timerCount, setTimerCount ] = useState<number>(duration)
+  const [ fadeDown, setFadeDown ] = useState<boolean>(false)
+  const [ isHideLoader, setIsHideLoader ] = useState<boolean>(false)
 
   useEffect(() => {
-    let timer: NodeJS.Timeout | null = null;
+    let timer: NodeJS.Timeout | null = null
 
     if (timerCount > 0) {
       timer = setTimeout(() => {
-        setTimerCount((prevCount) => prevCount - 1);
+        setTimerCount((prevCount) => prevCount - 1)
       }, 1000);
     } else {
-      // Timer has stopped
-      setFadeDown(true);
+      setFadeDown(true)
+      setTimeout(() => {
+        setIsHideLoader(true)
+      }, 500)
     }
 
     return () => {
       if (timer) {
-        clearTimeout(timer);
+        clearTimeout(timer)
       }
     };
-  }, [timerCount]);
+  }, [timerCount])
   
-  const containerStyle = `fixed z-[9999] bg-light dark:bg-dark top-0 left-0 right-0 bottom-0 flex justify-center items-center transition-opacity duration-500 ${ fadeDown ? 'opacity-0' : 'opacity-100' }`
+  const containerStyle = `fixed z-[9999] bg-light dark:bg-dark top-0 left-0 right-0 bottom-0 flex justify-center items-center transition-opacity duration-500 ${ fadeDown ? 'opacity-0' : 'opacity-100' } ${ isHideLoader ? 'invisible' : 'visible' }`
 
   return (
     <>
