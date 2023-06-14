4e738186972ebd0d60a41322c95d9b666ca930f7
Akkharawit Somwong <akk.somwong@gmail.com>
2023-06-14 21:00:02 +0700

    Update EditorArea.tsx

--- src/components/EditorArea.tsx
+++ src/components/EditorArea.tsx
@@ -56,98 +56,75 @@ const EditorArea: React.FC<EditorAreaProps> = ({ id, defaultHeight, ...props }) 
     }
   }, [height, defaultHeight])
 
-  const handleBoldClick = () => {
-    const textarea = textareaRef.current;
-    if (textarea) {
-      const selectionStart = textarea.selectionStart;
-      const selectionEnd = textarea.selectionEnd;
-    
-      const selectedText = content.slice(selectionStart, selectionEnd);
-      const formattedText = `<b>${selectedText}</b>`;
-    
-      const newContent =
-        content.slice(0, selectionStart) +
-        formattedText +
-        content.slice(selectionEnd);
-    
-      setContent(newContent);
-    
-      textarea.setSelectionRange(
-        selectionStart,
-        selectionStart + formattedText.length
-      )
-
-      textarea.focus()
+  const modifyText = (command: string, defaultUi: boolean, value: string | undefined) => {
+    if (command === 'superscript' || command === 'subscript') {
+      var selection = window.getSelection();
+      if (selection) {
+        var range = selection.getRangeAt(0);
+        var span = document.createElement('span');
+        
+        if (command === 'superscript') {
+          span.style.verticalAlign = 'super';
+        } else if (command === 'subscript') {
+          span.style.verticalAlign = 'sub';
+        }
+        
+        range.surroundContents(span);
+        selection.removeAllRanges();
+        selection.addRange(range);
+      }
+    } else {
+      document.execCommand(command, false, undefined);
     }
   }
 
-  const handleItalicClick = () => {
-    const textarea = textareaRef.current;
-    if (textarea) {
-      const selectionStart = textarea.selectionStart;
-      const selectionEnd = textarea.selectionEnd;
-    
-      const selectedText = content.slice(selectionStart, selectionEnd);
-      const formattedText = `<em>${selectedText}</em>`;
-    
-      const newContent =
-        content.slice(0, selectionStart) +
-        formattedText +
-        content.slice(selectionEnd);
-    
-      setContent(newContent);
-    
-      textarea.setSelectionRange(
-        selectionStart,
-        selectionStart + formattedText.length
-      )
-
-      textarea.focus()
-    }
+  const handleOptionClick = (option: string) => {
+    modifyText(option, false, undefined)
   }
 
   return (
     <section className="flex flex-col gap-[16px]">
     <section id="editor-menu" className="flex flex-auto items-center gap-[20px] bg-dark-500/5 rounded-sm p-[10px] backdrop-blur-sm">
       <section id="text" className="flex flex-auto gap-[5px] w-max max-w-max">
-        <span onClick={handleBoldClick} className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
+        <button onClick={() => handleOptionClick('bold')} className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
           <RiBold/>
-        </span>
-        <span onClick={handleItalicClick} className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
+        </button>
+        <button onClick={() => handleOptionClick('italic')} className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
           <RiItalic/>
-        </span>
-        <span className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
+        </button>
+        <button onClick={() => handleOptionClick('underline')} className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
           <RiUnderline/>
-        </span>
-        <span className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
+        </button>
+        <button onClick={() => handleOptionClick('strikethrough')} className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
           <RiStrikethrough/>
-        </span>
-        <span className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
+        </button>
+        <button onClick={() => handleOptionClick('superscript')} className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
           <RiSuperscript/>
-        </span>
-        <span className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
+        </button>
+        <button onClick={() => handleOptionClick('subscript')} className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
           <RiSubscript/>
-        </span>
-        <span className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
+        </button>
+        <button className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
           <RiLink/>
-        </span>
+        </button>
       </section>
       <section id="align" className="flex flex-auto gap-[5px] w-max">
-        <span className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
+        <button className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
           <RiAlignLeft/>
-        </span>
-        <span className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
+        </button>
+        <button className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
           <RiAlignCenter/>
-        </span>
-        <span className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
+        </button>
+        <button className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
           <RiAlignRight/>
-        </span>
-        <span className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
+        </button>
+        <button className="bg-dark-500/0 hover:bg-dark-500/5 rounded-sm p-[8px] transition-all duration-300 ease-in-out">
           <RiAlignJustify/>
-        </span>
+        </button>
       </section>
     </section>
-    <textarea
+    <div contentEditable={true} className='w-full bg-light-400/80 p-[16px] outline-none resize-none overflow-hidden'></div>
+    {/* <textarea
       id={id}
       ref={textareaRef}
       value={content}
@@ -155,7 +132,7 @@ const EditorArea: React.FC<EditorAreaProps> = ({ id, defaultHeight, ...props }) 
       className="w-full bg-light-400/80 p-[16px] outline-none resize-none overflow-hidden"
       {...props}
     />
-    <div dangerouslySetInnerHTML={{ __html: content }}/>
+    <div dangerouslySetInnerHTML={{ __html: content }}/> */}
   </section>
 
   )
